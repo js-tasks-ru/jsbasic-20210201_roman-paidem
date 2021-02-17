15b3af583dc74a06f16eea95204e27b2b04c9acb
Roman Paidem <romanramzespaydem@gmail.com>
2021-02-17 23:01:33 +0200

    checking an object for emptiness

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,8 @@
 function isEmpty(obj) {
   // ваш код...
+  for (let prop in obj) {
+    // если тело цикла начнет выполняться - значит в объекте есть свойства
+    return false;
+  }
+  return true;
 }
